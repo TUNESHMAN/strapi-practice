de6d882d9392834b82902d55a2f11218183736c1
Babatunde <babatundea15@gmail.com>
2021-01-14 20:12:23 +0100

    setup set up strapi app

---
+++ practice/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '07dc448c961f324542ec5429a842bbe9'),
+    },
+  },
+});
---
+++ practice/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1c7e180f-6fae-4bb0-823c-6f8b0094cac5'
+};
