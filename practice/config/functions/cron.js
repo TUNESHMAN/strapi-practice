e11ea0bd1df43ea84e6672a4e71fee7b01861e49
Babatunde <babatundea15@gmail.com>
2021-01-15 11:12:32 +0100

    retrieve data from endpoint

--- practice/config/functions/cron.js
+++ practice/config/functions/cron.js
@@ -1,4 +1,6 @@
-'use strict';
+"use strict";
+
+const fetch = require("node-fetch");
 
 /**
  * Cron config that gives you an opportunity
@@ -15,7 +17,20 @@ module.exports = {
    * Simple example.
    * Every monday at 1am.
    */
-  // '0 1 * * 1': () => {
-  //
-  // }
+  "* * * * *":  async() => {
+    // console.log(new Date());  
+    
+    const response = await fetch(
+      'https://parseapi.back4app.com/classes/City/10?keys=name',
+      {
+        headers: {
+          'X-Parse-Application-Id': 'mxsebv4KoWIGkRntXwyzg6c6DhKWQuit8Ry9sHja', // This is the fake app's application id
+          'X-Parse-Master-Key': 'TpO0j3lG2PmEVMXlKYQACoOXKQrL3lwM0HwR9dbH', // This is the fake app's readonly master key
+        }
+      }
+    );
+    const data = await response.json(); // Here you have the data that you need
+    console.log(JSON.stringify(data, null, 2, "Hello"));
+    
+  },
 };
--- practice/config/server.js
+++ practice/config/server.js
@@ -1,6 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  "cron": {
+    "enabled": true
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '07dc448c961f324542ec5429a842bbe9'),
