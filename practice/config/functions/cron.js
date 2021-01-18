a818ea20280e7c4c7c9e4f5f56abc5474b7ca4ad
Babatunde <babatundea15@gmail.com>
2021-01-18 08:48:40 +0100

    pushed final code

--- practice/config/functions/cron.js
+++ practice/config/functions/cron.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const fetch = require("node-fetch");
-
 /**
  * Cron config that gives you an opportunity
  * to run scheduled jobs.
@@ -17,20 +15,6 @@ module.exports = {
    * Simple example.
    * Every monday at 1am.
    */
-  "* * * * *":  async() => {
-    // console.log(new Date());  
-    
-    const response = await fetch(
-      'https://parseapi.back4app.com/classes/City/10?keys=name',
-      {
-        headers: {
-          'X-Parse-Application-Id': 'mxsebv4KoWIGkRntXwyzg6c6DhKWQuit8Ry9sHja', // This is the fake app's application id
-          'X-Parse-Master-Key': 'TpO0j3lG2PmEVMXlKYQACoOXKQrL3lwM0HwR9dbH', // This is the fake app's readonly master key
-        }
-      }
-    );
-    const data = await response.json(); // Here you have the data that you need
-    console.log(JSON.stringify(data, null, 2, "Hello"));
-    
-  },
+  // * * * * *
+  "": () => {strapi.config.functions.cities();},
 };
---
+++ practice/config/functions/weather.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const env = process.env.weather_id;
+
+module.exports = async () => {
+  const response = await axios.post(
+    "https://countriesnow.space/api/v0.1/countries/cities",
+    {
+      country: "germany",
+    }
+  );
+  const city_name = response.data.data;
+
+  for (let index = 0; index < city_name.length; index++) {
+    const element = city_name[index];
+    const weather_response = await axios.post(
+      `http://api.openweathermap.org/data/2.5/weather?q=${element}&appid=${env}`
+    );
+    const weather_details = weather_response.data.main.temp;
+
+    await strapi.query("weathers").create({
+      temperature: weather_details,
+    });
+  }
+};
